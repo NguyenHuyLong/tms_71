805f1f2b2389d2c990a4df1f49d59b2f22efa91f
NguyenHuyLong <nguyen.huy.long@framgia.com>
2017-01-07 00:06:01 +0700

    Create Subject and Task

---
+++ app/controllers/supervisors/subjects_controller.rb
@@ -0,0 +1,57 @@
+class Supervisors::SubjectsController < ApplicationController
+  before_action :find_subject, except: [:index, :new, :create]
+
+  def index
+    @subjects = Subject.order(created_at: :desc).paginate page: params[:page]
+  end
+
+  def show
+    @tasks = @subject.tasks
+  end
+
+  def new
+    @subject = Subject.new
+    Settings.task.times{@subject.tasks.build}
+  end
+
+  def create
+    @subject = Subject.new subject_params
+    if @subject.save
+      flash[:info] = t "flash.success.create_subject"
+      redirect_to supervisors_subjects_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @subject.update_attributes subject_params
+      redirect_to supervisors_subject_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @subject.destroy
+    flash[:success] = t "flash.delete.delete_subject"
+    redirect_to supervisors_subjects_path
+  end
+
+  private
+  def subject_params
+    params.require(:subject).permit :name, :description,
+      tasks_attributes: [:id, :name, :description, :_destroy]
+  end
+
+  def find_subject
+    @subject = Subject.find_by id: params[:id]
+    if @subject.nil?
+      flash[:danger] = t "flash.existed_subject"
+      redirect_to supervisors_subject_path
+    end
+  end
+end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,7 +1,11 @@
 class Subject < ApplicationRecord
   has_many :course_subjects
   has_many :course, through: :course_subjects
-  has_many :tasks
+  has_many :tasks, dependent: :destroy, inverse_of: :subject
   has_many :user_subjects
   has_many :users, through: :user_subjects
+
+  validates :name, presence: true
+  accepts_nested_attributes_for :tasks, allow_destroy: true,
+    reject_if: -> tasks {tasks["name"].blank?}
 end
