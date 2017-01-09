60ed398edc6e483fed5ec43f66a3d6c8753af627
trinhvantu <trinhvantu93@gmail.com>
2017-01-09 13:43:57 +0700

    show, edit profile and list of courses for the user

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   get "/pages/*page", to: "static_pages#show"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
-  resources :users, except: [:edit, :update, :destroy]
-
+  delete "/logout", to: "sessions#destroy" 
+  resources :users, except: [:destroy]
+  
   namespace :supervisors do
     resources :subjects
   end
