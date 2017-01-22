daec831a9cdcded84539389714e19c0951c55ce1
NguyenHuyLong <nguyen.huy.long@framgia.com>
2017-01-22 14:42:36 +0700

    Fix bug 1st

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
   def new
-  end 
+  end
 
   def create
     user = User.find_by email: params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to user
+      redirect_to root_path
     else
       flash.now[:danger] = t "flash.login"
       render :new
